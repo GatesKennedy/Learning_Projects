17ec78c79b91687d05db49bcc986649aae8c6d25
GatesKennedy <Conor@esccollective.com>
2018-05-24 23:20:07 -0700

    new Bridger's Birthday Present

---
+++ HappyBirthdaybridgerbaby/HappyBirthdayBridgerBaby/BridgerBaby.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BridgerBaby.aspx.cs" Inherits="HappyBirthdayBridgerBaby.BridgerBaby" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>HappyBirthdayBridgerBallbach</title>
+    <link rel="stylesheet" href="stylesheet1.css" type="text/css" />
+    <style>
+        body {
+            background-color: #090909;
+            color: #f8f8f8;
+            text-align: center;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .header {
+            font-size: larger;
+            padding: 20px;
+            margin-top: 20px;
+            font-family: 'brutal-tooth';
+        }
+
+        .picContainer {
+            padding: 10px;
+        }
+
+        .ButtonContainer {
+            margin: 10px;
+            padding: 10px;
+            color: #f9f9f9;
+        }
+
+        #Button1 {
+            font-size: 17px;
+            font-family: Arial, Helvetica, sans-serif;
+            color: #f9f9f9;
+            font-style: italic;
+        }
+
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <div class="header">
+            Happy Birthday <br /> Bridger Baby<br />
+            5/25/18<br />
+        </div>
+
+        <div class="picContainer">
+            <asp:Image ID="PicRock" runat="server" ImageUrl="~/App_Data/Rock1.jpg" Visible="False" />
+            <br />
+            <asp:Button ID="Button1" runat="server" BackColor="#FF6666" BorderColor="#990033" Text="How Much Do We Love You?" OnClick="Button1_Click" />
+        </div>
+     
+        <div class="LabelContainer">
+            <asp:Label ID="ResultLabel" runat="server"></asp:Label>
+        </div>
+
+    </form>
+</body>
+</html>
+
---
+++ HappyBirthdaybridgerbaby/HappyBirthdayBridgerBaby/BridgerBaby.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HappyBirthdayBridgerBaby
+{
+    public partial class BridgerBaby : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+         * I love <bridgerTrait> so much I'd <action1>
+         * my <valuableThing> and <action2> for <timeVal>
+         * <Ending>
+         * -<fName> <lName>
+         */
+
+        private static Random _rando = new Random();
+
+        List<string> madValues = new List<string>();
+
+        List<string> traits = new List<string>
+        {
+            "the way you get excited about things",
+            "the smirk you make when land an 'edgy' joke",
+            "trait3",
+            "trait4",
+            "trait5",
+            "trait6"
+        };
+
+        List<string> action1 = new List<string>
+        {
+            "'earn it' with",
+            "forget about",
+            "breast feed",
+            "donate",
+            "penguin wrestle",
+            "kill a doe with"
+        };
+
+        List<string> valuableThing = new List<string>
+        {
+            "first born child",
+            "studded belts",
+            "dirty jokes"
+
+        };
+
+        List<string> action2 = new List<string>
+        {
+            "play puke dragon",
+            "swear of Big Buck",
+
+        };
+
+        List<string> timeVal = new List<string>
+        {
+            "8 seconds",
+            "12 minutes",
+            "83 days",
+            "every day for 11 years",
+            "as long as I live"
+        };
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int rando1 = _rando.Next(0, 8);
+            int rando2 = _rando.Next(0, 8);
+            int rando3 = _rando.Next(0, 8);
+            int rando4 = _rando.Next(0, 8);
+            int rando5 = _rando.Next(0, 8);
+            int rando6 = _rando.Next(0, 8);
+
+            for (int6 i = 0; i < 6; i++)
+            {
+                int rando = _rando.Next(0, 8);
+                var madTemp = 
+
+            }
+            
+
+        }
+    }
+}
